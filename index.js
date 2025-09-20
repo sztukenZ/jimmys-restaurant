366371bd0568612cbc2a611e0cb2aa9c549bac60
Chris <krzysztof.jerzy.sztuk@gmail.com>
2025-09-20 12:44:00 +0200

    Modal form working as expected

--- index.js
+++ index.js
@@ -3,14 +3,47 @@ import { menuArray } from "./data.js";
 const productSection = document.getElementById("products");
 const orderSection = document.getElementById("order");
 const orderedItems = [];
+const overlay = document.querySelector(".overlay");
+const modal = document.getElementById("modal");
+const payBtn = document.getElementById("pay-btn");
+
+function openModal() {
+  document.body.classList.add("modal-open");
+}
+
+function closeModal() {
+  document.body.classList.remove("modal-open");
+}
+
+overlay.addEventListener("click", closeModal);
+
+window.addEventListener("keydown", (e) => {
+  if (e.key === "Escape") closeModal();
+});
 
 document.addEventListener("click", function (e) {
   if (e.target.dataset.product) {
     addItemToOrder(e.target.dataset.product);
   } else if (e.target.dataset.remove) {
     removeProduct(e.target.dataset.remove);
+  } else if (e.target.id === "order-btn") {
+    openModal();
   }
 });
+
+// TODO: Get data from the form and input name
+
+function pay() {
+  payBtn.addEventListener("click", function (event) {
+    event.preventDefault();
+    let form = document.forms[0];
+    console.log(form);
+    closeModal();
+    orderSection.innerHTML = `
+      <h3>Thanks ${name}. Your order is on its way!</h3>
+    `;
+  });
+}
 
 function addItemToOrder(itemId) {
   const targetMenuItem = menuArray.filter(function (item) {
@@ -51,7 +84,7 @@ function renderOrderSection() {
         <h3>Total price:</h3>
         <h4>$${totalPrice}</h4>
       </div>
-  <button class="green-btn">Complete order</button>
+  <button class="green-btn" id="order-btn">Complete order</button>
   `;
 
   orderSection.innerHTML = orderSectionHtml;
